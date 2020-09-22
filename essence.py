9ec1a1957b3db71181d5a6ba55a3b5b48505ccdb
Philip Caputo Two.0 <philip@two-0.org>
2020-09-22 13:27:58 -0700

    the essence of the program

---
+++ essence.py
@@ -0,0 +1,47 @@
+# @Author: two_0
+# @Date:   22-09-2020
+# @Email:  philip@two-0.org
+# @Project: Python Tutorial
+# @Last modified by:   two_0
+# @Last modified time: 22-09-2020
+# @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
+# @Copyright: Copyright 2020 © - All Rights Reserved
+#     ___ __ ._`.*.'_._ ____ רףאל
+#    . +  * .\   o.* `.`. +.  א .
+#   *  .ת' '  \^/|  `. * .  * `
+#             \V/ . +
+#    יהוה      /_\  .`.
+#   ======== _/W\_ =אדני:By: Two.0:.*
+def personal_independence(income, ave_expenses):
+    if type(income) == type('PASSIVE'):
+        return income > ave_expenses
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#The Essence of a Program
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+'''
+A program is a specifically written letter to the
+CPU of the computer.  It tells that CPU exactly
+to the 'T' what to do and exactly how to do it.
+#
+To whom it may concern,
+RE: Recipe for Stone Soup
+Link: https://www.tasteofhome.com/recipes/stone-soup/
+#
+A program can also be thought of as a series of specific steps
+that solves a specific problem.
+Step 1)...
+Step 2)...
+if this, go to Step 3) else repeat Step 2)...
+'''
+def step_1():
+    print('Hello World!')
+def step_2():
+    print('3+4='+str(3+4))
+def step_3(input):
+    print('Hello '+str(input))
+#these definitions
+step_1()
+step_2()
+step_3('Python!')
