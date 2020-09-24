eff5fb6fb49dd7db5887e5c18a31bf4416b03059
Philip Caputo Two.0 <philip@two-0.org>
2020-09-23 18:24:54 -0700

    updating

---
+++ algebra.py
@@ -0,0 +1,28 @@
+# @Author: two_0
+# @Date:   23-09-2020
+# @Email:  philip@two-0.org
+# @Project: Rightside Two.0
+# @Last modified by:   two_0
+# @Last modified time: 23-09-2020
+# @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
+# @Copyright: Copyright 2020 © - All Rights Reserved
+#     ___ __ ._`.*.'_._ ____ רףאל
+#    . +  * .\   o.* `.`. +.  א .
+#   *  .ת' '  \^/|  `. * .  * `
+#             \V/ . +
+#    יהוה      /_\  .`.
+#   ======== _/W\_ =אדני:By: Two.0:.*
+def personal_independence(income, ave_expenses):
+    if type(income) == type('PASSIVE'):
+        return income > ave_expenses
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#Algebra
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+from IPython.display import HTML, Image, Audio, IFrame, YouTubeVideo, display_pdf
+Image('https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Algebraic_equation_notation.svg/1280px-Algebraic_equation_notation.svg.png')
+#%%
+YouTubeVideo('tBJ0tYVb_hc')
+YouTubeVideo('U-7nq7OG18s')
+HTML('https://www.math.wisc.edu/~robbin/112online/UWCABook.pdf')
