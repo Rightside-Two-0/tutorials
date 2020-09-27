4440c93c88b1fc2c4928bce9fda0725570838ee8
Philip Caputo Two.0 <philip@two-0.org>
2020-09-27 02:33:56 -0700

    updating

---
+++ tens_magic.py
@@ -0,0 +1,43 @@
+# @Author: two_0
+# @Date:   25-09-2020
+# @Email:  philip@two-0.org
+# @Project: Python Challenge
+# @Last modified by:   two_0
+# @Last modified time: 25-09-2020
+# @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
+# @Copyright: Copyright 2020 © - All Rights Reserved
+#     ___ __ ._`.*.'_._ ____ רףאל
+#    . +  * .\   o.* `.`. +.  א .
+#   *  .ת' '  \^/|  `. * .  * `
+#             \V/ . +
+#    יהוה      /_\  .`.
+#   ======== _/W\_ =אדני:By: Two.0:.*
+def personal_independence(income, ave_expenses):
+    if type(income) == type('PASSIVE'):
+        return income > ave_expenses
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#Tens Magic
+#%%
+from IPython.display import *
+Latex('$21 * 10 = 210$')
+Latex('$21 * 10 = 21+21+21+21+21+21+21+21+21+21$')
+Latex('$21*10 = 210$')
+21*10
+#%%
+def ten_magic(input):
+    return Latex('$'+str((str(input)+'+')*(10))[:-1]+'='+str(input*10)+'$')
+#%%
+ten_magic(21)
+ten_magic(22)
+#%%
+ten_magic(3)
+ten_magic(5)
+ten_magic(22000)
+ten_magic(220)
+ten_magic(22000963)
+ten_magic(17)
+ten_magic(30)
+ten_magic(22)
+ten_magic(50)
+ten_magic(220009)
