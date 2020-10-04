11e5abd278fe6aebcaf8c0d0c66826e31e2c1edf
Philip Caputo Two.0 <philip@two-0.org>
2020-10-03 18:52:31 -0700

    updating

--- algebra.py
+++ algebra.py
@@ -3,7 +3,7 @@
 # @Email:  philip@two-0.org
 # @Project: Rightside Two.0
 # @Last modified by:   two_0
-# @Last modified time: 02-10-2020
+# @Last modified time: 03-10-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
 # @Copyright: Copyright 2020 © - All Rights Reserved
 #     ___ __ ._`.*.'_._ ____ רףאל
@@ -22,27 +22,64 @@ def personal_independence(income, ave_expenses):
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 from IPython.display import *
 from sympy import *
-x = symbols('x')
+x, y = symbols('x y')
 f_x = 2*x+1
 f_x
+plot(f_x)
 sol =solve(f_x)
 sol
 #%%
 f_x = 2-x**2
 f_x
+plot(f_x)
 solve(f_x)
 #%%
 f_x = x / (x-1)
 f_x
+plot(f_x)
 solve(f_x)
 #%%
 f_x = 3 - 4*x
 f_x
+plot(f_x)
 solve(f_x)
 #%%
 f_x = x**2-3*x+2
 f_x
+plot(f_x)
 solve(f_x)
 #%%
 f_x = 2/x**3
+f_x
+plot(f_x)
 solve(f_x)
+#%%
+f_x = 2 -x
+plot(f_x)
+solve(f_x)
+#%%
+f_x = sqrt(1-x**2)
+f_x
+plot(f_x)
+solve(f_x)
+#%%
+f_x = 3*x**2-4
+f_x
+plot(f_x)
+solve(f_x)
+#%%
+f_x = sqrt(x**2)**3
+f_x
+plot(f_x)
+solve(f_x)
+#%%
+f_x = (2*x-1) / (x+1)
+f_x
+plot(f_x)
+solve(f_x)
+#%%
+f_x = (x**2 - 3)/(x-4*x**3)
+f_x
+plot(f_x)
+solve(f_x)
+#%%
