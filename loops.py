28cec85ace6de0c765e06ece656cfa129ec285ea
Philip Caputo Two.0 <philip@two-0.org>
2020-10-09 09:45:39 -0700

    difference between while & for loops

---
+++ loops.py
@@ -0,0 +1,34 @@
+# @Author: two_0
+# @Date:   09-10-2020
+# @Email:  philip@two-0.org
+# @Project: Python Challenge
+# @Last modified by:   two_0
+# @Last modified time: 09-10-2020
+# @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
+# @Copyright: Copyright 2020 © - All Rights Reserved
+#     ___ __ ._`.*.'_._ ____ רףאל
+#    . +  * .\   o.* `.`. +.  א .
+#   *  .ת' '  \^/|  `. * .  * `
+#             \V/ . +
+#    יהוה      /_\  .`.
+#   ======== _/W\_ =אדני:By: Two.0:.*
+def personal_independence(income, ave_expenses):
+    if type(income) == type('PASSIVE'):
+        return income > ave_expenses
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#Loops
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+from IPython.display import *
+data = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+#%%
+#for loop 1st
+for item in data:
+    print(item)
+#%%
+#while loop 2nd
+counter = 0
+while counter <= len(data)-1:
+    print(data[counter])
+    counter += 1
