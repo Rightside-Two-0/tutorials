47cf81438a122f034447ded7fcd78c12127087d3
Philip Caputo Two.0 <philip@two-0.org>
2020-10-18 06:31:17 -0700

    part 1 exercises

--- exercises/chapter_three.py
+++ exercises/chapter_three.py
@@ -1,7 +1,7 @@
 # @Author: two_0
 # @Date:   16-10-2020
 # @Email:  philip@two-0.org
-# @Project: Python Challenge
+# @Project: Python Power Series
 # @Last modified by:   two_0
 # @Last modified time: 18-10-2020
 # @License: https://github.com/Rightside-Two-0/Rightside_Two.0/blob/master/LICENSE
@@ -15,4 +15,60 @@
 def personal_independence(income, ave_expenses):
     if type(income) == type('PASSIVE'):
         return income > ave_expenses
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#Python Power
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+from IPython.display import *
+from sympy import *
+import math
+import matplotlib.pyplot as plt
+#programming == Living algebra
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+Image('/home/two_0/projects/tutorials/exercises/python.png')
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+Image('/home/two_0/projects/tutorials/exercises/python.png', width=35, height=35)
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+YouTubeVideo('7bnzid1eV3g', start=300)
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%%md
+#Hello Python Exercises
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#Mailing address
+def get_address():
+    name = 'Rightside Two_0'
+    address = '4747 E 1st'
+    city = 'Tucson'
+    state = 'AZ'
+    zip_code = '85711'
+    return name+'\n'+address+'\n'+city+', '+state+' '+zip_code
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+print(get_address())
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+def hello_user():
+    name = input('Please enter your name')
+    return 'Hello '+name+', it is nice to meet you!'
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+hello_user()
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#Area of a room
+def get_room_area():
+    width_room = float(input('Enter the width of room'))
+    length_rooom = float(input('Enter the length of room'))
+    return width_room*length_rooom
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_room_area()
+assert get_room_area() == 13*42
+#%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
