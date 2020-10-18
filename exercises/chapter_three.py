0803c3c3d6040fc710eea37e3976d2fd37bc40ad
Philip Caputo Two.0 <philip@two-0.org>
2020-10-18 08:58:24 -0700

    part 2 basic fun exercises

--- exercises/chapter_three.py
+++ exercises/chapter_three.py
@@ -76,3 +76,46 @@ def hello_user():
 print(hello_user())
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# Area of a room
+def get_area_room():
+    width = float(input('Enter width of room'))
+    length = float(input('Enter length of room'))
+    return width*length
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_area_room()
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# get field aread given in feet but return in acres 43560
+def get_field_area():
+    width = float(input('Enter the width in feet'))
+    length = float(input('Enter length in feet'))
+    return (width*length)/43560
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_field_area()
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# Bottle deposit return <= 1L:.10 & > 1L:.25
+def get_return():
+    less_than = int(input('Enter less than or equal to 1 liter'))
+    more_than = int(input('Enter more than 1 liter'))
+    return '$'+'{0:,.2f}'.format(less_than*.1+more_than*.25)
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_return()
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# Tax & tip
+def get_tax_tip():
+    tax_rate = 0.0825
+    tip_rate = 0.18
+    cost_meal = float(input('Enter the cost of the meal'))
+    tip_amount = tip_rate*cost_meal
+    tax_amount = tax_rate*cost_meal
+    total_cost_meal = cost_meal+tip_amount+tax_amount
+    g = lambda var : '{0:,.2f}'.format(var)
+    return f'Your tip amount is: {g(tip_amount)}\nYour tax amount is: {g(tax_amount)}\nYour total due is: {g(total_cost_meal)}'
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+print(get_tax_tip())
