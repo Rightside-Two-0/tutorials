50ca135e60c2a24b5530291aaf75f6e3d7e9bfb3
Philip Caputo Two.0 <philip@two-0.org>
2020-10-23 01:26:50 -0700

    updating

--- exercises/chapter_3_5.py
+++ exercises/chapter_3_5.py
@@ -45,7 +45,46 @@ def get_total_weight():
     gizmo_weight = 112
     widget_number = int(input('Enter number of widgets sold: '))
     gizmo_number = int(input('Enter number of gizmos sold: '))
-    
-
+    return f'Total weight of the parts is: {(widget_number*widget_weight+gizmo_weight*gizmo_number)/1000}'
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_total_weight()
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# arithmetic
+def arithmetic():
+    a = int(input('Enter a valid integer'))
+    b = int(input('Enter a valid integer'))
+    sum_str = f'The sum of a and b is {a+b}\n'
+    difference_str = f'The difference when b is subtracted from a is {a-b}\n'
+    product_str = f'The product of a and b is {a*b}\n'
+    quotient_str = f'The quotient when a is divided by b is {a//b}\n'
+    remainder_str = f'The remainder when a is divided by b is {a%b}\n'
+    result_log = f'The result of log_10(a) is {math.log10(a)}\n'
+    result_exp = f'The result of a^b is {a**b}\n'
+    return sum_str+difference_str+product_str+quotient_str+remainder_str+result_log+result_exp
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+print(arithmetic())
+a=42
+b=13
+a+b
+a-b
+a*b
+a//b
+a%b
+math.log10(a)
+a**b
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+# Fuel efficiency
+def get_conversion_rate():
+    #(100*3.785411784)/(1.609344*mpgs)
+    mpgs = int(input('Enter the US MPGs that was used:'))
+    canada_eqv = (100*3.785411784)/(1.609344*mpgs)
+    return '{0:,.2f}'.format(canada_eqv)
+#%%
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+get_conversion_rate()
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
--- exercises/chapter_three.py
+++ exercises/chapter_three.py
@@ -62,9 +62,11 @@ def get_address():
     state = 'AZ'
     zip_code = '85711'
     return name+'\n'+address+'\n'+city+', '+state+' '+zip_code
+
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 print(get_address())
+
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 # Hello user!
@@ -94,6 +96,12 @@ def get_field_area():
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 get_field_area()
+
+
+
+
+
+
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 # Bottle deposit return <= 1L:.10 & > 1L:.25
@@ -121,3 +129,135 @@ def get_tax_tip():
 print(get_tax_tip())
 #%%
 #~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Sum of the 1st 'n' integers
+def sum_to():
+    up_to = int(input('Please provide an integer 100 or less'))
+    total = 0
+    for item in range(1, up_to+1):
+        total += item
+    return total
+#%%
+sum_to()
+assert sum_to() == 100*101/2
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Widgets & gizmos
+def get_total_weight():
+    widget_weight = 75
+    gizmo_weight = 112
+    widget_number = int(input('Enter number of widgets sold:'))
+    gizmo_number = int(input('Enter number of gizmos sold:'))
+    return f'Total weight of the parts is: {(widget_number*widget_weight+gizmo_number*gizmo_weight)/1000} kilograms'
+#%%
+get_total_weight()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Arithmetic
+def arithmetic():
+    a = int(input('Enter a valid integer'))
+    b = int(input('Enter a another valid integer'))
+    sum_str = f'The sum of a and b is {a+b}\n'
+    difference_str = f'The difference when b is subtracted from a is {a-b}\n'
+    product_str = f'The product of a and b is {a*b}\n'
+    quotient_str = f'The quotient when a is divided by b is {a//b}\n'
+    remainder_str = f'The remainder when a is divided by b is {a%b}\n'
+    result_log = f'The result of log_10(a) is {math.log10(a)}\n'
+    result_exp = f'The result of a^b is {a**b}\n'
+    return sum_str+difference_str+product_str+quotient_str+remainder_str+result_log+result_exp
+#%%
+print(arithmetic())
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Fuel efficiency
+def get_conversion_rate():
+    mpgs = int(input('Enter the US MPGs that was used:'))
+    canada_eqv = (100*3.785411784)/(1.609344*mpgs)
+    return '{0:,.2f}'.format(canada_eqv)
+#%%
+get_conversion_rate()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Distance between 2 points on Earth
+#distance = 6371.01×arccos(sin(t1)×sin(t2)+cos(t1)×cos(t2)×cos(g1 − g2))
+def get_distance():
+    t1 = math.radians(float(input('Enter Latitude 1 as degrees')))
+    g1 = math.radians(float(input('Enter Longitude 1 as degrees')))
+    t2 = math.radians(float(input('Enter Latitude 2 as degrees')))
+    g2 = math.radians(float(input('Enter Longitude 2 as degrees')))
+    return 6371.01*math.acos(math.sin(t1)*math.sin(t2)+math.cos(t1)*math.cos(t2)*math.cos(g1-g2))
+#%%
+get_distance()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Making change
+def get_change():
+    cents = int(input('Enter the number of cents to be returned'))
+    num_toonies = 0
+    num_loonies = 0
+    num_quaters = 0
+    num_dimes = 0
+    num_nickles = 0
+    num_pennies = 0
+    while cents > 0:
+        update = 0
+        if cents % 200 == 0:
+            num_toonies += 1
+            update = 200
+        elif cents % 100 == 0:
+            num_loonies += 1
+            update = 100
+        elif cents % 25 == 0:
+            num_quaters += 1
+            update = 25
+        elif cents % 10 == 0:
+            num_dimes += 1
+            update = 10
+        elif cents%5 ==0:
+            num_nickles += 1
+            update = 5
+        elif cents%1 == 0:
+            num_pennies += 1
+            update = 1
+        cents = cents-update
+    return {'Toonies':num_toonies, \
+            'Loonies':num_loonies, \
+            'Quaters':num_quaters, \
+            'Dimes':num_dimes, \
+            'Nickles':num_nickles, \
+            'Pennies':num_pennies}
+#%%
+get_change()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Height units
+def convert_centi():
+    feet = int(input('Please enter height in feet'))
+    inches = int(input('please enter the height in inches'))
+    return feet*12*2.54+inches*2.54
+#%%
+convert_centi()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Area and volume
+def get_area_volume():
+    radius = float(input('Enter the radius of your circle'))
+    area = math.pi*radius*radius
+    assert radius**2 == radius*radius
+    volume = (4/3)*math.pi*radius*radius*radius
+    return area, volume
+#%%
+get_area_volume()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Heat capicity
+def get_head_capicity():
+    mass = float(input('Enter the mass of the water'))
+    temp_delta = float(input('Enter the temperature change'))
+    energy = mass*4.186*temp_delta
+    return energy
+#%%
+get_head_capicity()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Volume of cylinder
+def get_cylinder_vol():
+    radius = float(input('Enter the radius of bottom'))
+    height = float(input('Enter the height'))
+    result = (math.pi*radius*radius)*height
+    return '{0:,.1f}'.format(round(result, 1))
+#%%
+get_cylinder_vol()
+#~~Rightside~Two.0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#%% Free fall
